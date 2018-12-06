04ca6604594cf8f93ef2a06698ae6dcdc4dd5077
Bulat Khabirov <khabiroff_b@me.com>
2018-12-06 17:20:07 +0300

    WIP parser 90%

--- src/main/scala/university/innopolis/mlang/program/Parser.scala
+++ src/main/scala/university/innopolis/mlang/program/Parser.scala
@@ -1,9 +1,8 @@
 package university.innopolis.mlang.program
 
 import scala.collection.JavaConverters._
-
 import org.antlr.v4.runtime._
-
+import org.antlr.v4.runtime.tree.TerminalNode
 import university.innopolis.mlang.program.ast._
 import university.innopolis.mlang.parser._
 import university.innopolis.mlang.parser.MlangParser._
@@ -35,50 +34,156 @@ object Parser {
 
     val program = parser.program()
 
-    val memory: Map[String, Expression] = program
-      .memoryBlock().valDecl().asScala
-      .map { i => (i.IDENTIFIER().toString, i.expression()) }.toMap
-      .mapValues { _ => UnaryExpression(IntLiteral(0)) }
-    val instructions: List[Statement] = program
-      .programBlock().statementBlock().statement().asScala
-      .map {
-        case i if i.command() != null =>
-          val command = i.command().moveCommand()
-          val params: Map[String, Operand] = Option(command.parameterList()).iterator
-            .flatMap(_.parameterDecl().asScala)
-            .map { i => i.IDENTIFIER().toString -> operand(i.operand()) }.toMap
-          val target = command.moveTarget()
-          MoveCommand(Identifier(target.IDENTIFIER().toString), params)
-        case i if i.assignStatement() != null =>
-          val assignment = i.assignStatement()
-          val left = assignment.expression(0)
-          val right = assignment.expression(1)
-          AssignmentStatement(expression(left), expression(right))
-        case i => println(i.getRuleIndex); ???
-      }.toList
+    val memory: Map[String, Expression] = this.memory(program.memoryBlock())
+    val instructions: List[Statement] =
 
     Program(memory, instructions)
   }
 
-  def operand(input: OperandContext): Operand = input match {
-    case i if i.IDENTIFIER() != null =>
-      Identifier(i.IDENTIFIER().toString)
-    case i => i.literal() match {
-      case j if j.INT_LIT() != null =>
-        IntLiteral(j.INT_LIT().toString.toInt)
-      case j if j.FLOAT_LIT() != null =>
-        FloatLiteral(j.FLOAT_LIT().toString.toDouble)
+  def memory(input: MemoryBlockContext): Map[String, Expression] = input.valDecl().asScala
+    .map { i => i.IDENTIFIER().getText -> expression(i.expression()) }
+    .toMap
+
+  def command(input: CommandContext): Command = {
+    val command = input.moveCommand()
+    val target = command.moveTarget() match {
+      case j if j.IDENTIFIER() != null =>
+        Identifier(j.IDENTIFIER().getText)
+      case j if j.typeExpression() != null =>
+        typeOperand(j.typeExpression())
     }
+
+    var parameters: Map[String, Operand] = Map.empty
+    if (command.parameterList() != null) {
+      parameters = this.parameters(command.parameterList())
+    }
+
+    MoveCommand(target, parameters)
   }
 
+  def statements(input: StatementBlockContext): StatementBlock = StatementBlock(
+    input.statement().asScala
+      .map {
+        case i if i.command() != null =>
+          command(i.command())
+        case i if i.assignStatement() != null =>
+          val assignExpressions = i.assignStatement().expression()
+          AssignmentStatement(expression(assignExpressions(0)), expression(assignExpressions(0)))
+        case i if i.ifStatement() != null =>
+          val ifStatement = i.ifStatement()
+          var elseStatements: Option[StatementBlock] = None
+
+          if (ifStatement.statementBlock().asScala.size > 1) {
+            elseStatements = Some(statements(ifStatement.statementBlock(1)))
+          }
+
+          IfStatement(
+            expression(ifStatement.expression()),
+            statements(ifStatement.statementBlock(0)),
+            elseStatements
+          )
+        case i if i.forStatement() != null =>
+          val forStatment = i.forStatement()
+
+          val forClause = forStatment.forClause()
+          val forClauseExpressions = forClause.range().expression()
+          var clauseID: Option[Identifier] = None
+          if (forClause.IDENTIFIER() != null) {
+            clauseID = Some(Identifier(forClause.IDENTIFIER().getText))
+          }
+          val clause = ForClause(
+            clauseID,
+            expression(forClauseExpressions(0)),
+            expression(forClauseExpressions(1))
+          )
+
+          ForStatement(clause, statements(forStatment.statementBlock()))
+      }
+      .toList
+  )
+
+  def parameters(input: ParameterListContext): Map[String, Operand] = input.parameterDecl().asScala
+    .map { i => i.IDENTIFIER().getText -> operand(i.operand()) }
+    .toMap
+
+  def typeOperand(input: TypeExpressionContext): TypeOperand = TypeOperand(Point, parameters(input.parameterList()))
+
+  def operand(input: OperandContext): Operand = input match {
+    case i if i.IDENTIFIER() != null =>
+      // simple identifier operands
+      Identifier(i.IDENTIFIER().toString)
+    case i if i.literal() != null => i.literal() match {
+      // literal operands
+      case j if j.INT_LIT() != null =>
+        // integer literals
+        IntLiteral(j.INT_LIT().toString.toInt)
+      case j if j.FLOAT_LIT() != null =>
+        // float literals
+        FloatLiteral(j.FLOAT_LIT().toString.toDouble)
+      case j if j.BOOLEAN_LIT() != null => j.BOOLEAN_LIT().getText match {
+        // bool literals
+        case "true" => BooleanLiteral(true)
+        case "false" => BooleanLiteral(false)
+      }
+      // string literals
+      case j if j.STRING_LIT() != null => StringLiteral(j.STRING_LIT().getText)
+    }
+    case i if i.expression() != null =>
+      // operands that are themselves expressions, i.e. (5 + 3)
+      ExpressionOperand(expression(i.expression()))
+    case i if i.typeExpression() != null =>
+      // type expressions
+      typeOperand(i.typeExpression())
+  }
+
+  /**
+    * Retrieves an [[Expression]] from a given [[ExpressionContext]] ANTLR node.
+    *
+    * @param input ANTLR's [[ExpressionContext]] node to map to [[Expression]].
+    * @return parsed [[Expression]] subclass.
+    */
   def expression(input: ExpressionContext): Expression = input match {
     case i if i.unaryExpr() != null =>
+      // unary
       val unaryExpr = i.unaryExpr()
       val unaryOps: List[UnaryOp] = unaryExpr.UNARY_OP().asScala.map(_.toString).map {
         case "!" => Not
       }.toList
 
       UnaryExpression(operand(unaryExpr.operand()), unaryOps)
+    case i if i.BINARY_OP() != null =>
+      // binary
+      val left = i.expression(0)
+      val right = i.expression(1)
+      val operator: BinOp = binaryOperator(i.BINARY_OP())
 
+      BinaryExpression(operator, expression(left), expression(right))
+    case i if i.IDENTIFIER() != null =>
+      // dot
+      val identifier = i.IDENTIFIER().getText
+      val left = i.expression(0)
+
+      DotExpression(expression(left), identifier)
+  }
+
+  /**
+    * Retrieves a [[BinOp]] from a given [[BINARY_OP]] terminal node.
+    *
+    * @param input terminal node from ANTLR, should be [[BINARY_OP]].
+    * @return parsed [[BinOp]] subclass.
+    */
+  def binaryOperator(input: TerminalNode): BinOp = input.getText match {
+    case "||" => Or
+    case "&&" => And
+    case "==" => Equal
+    case "!=" => NotEqual
+    case "<" => Less
+    case "<=" => LessOrEqual
+    case ">" => Greater
+    case ">=" => GreaterOrEqual
+    case "+" => Addition
+    case "-" => Minus
+    case "*" => Multiplication
+    case "/" => Division
   }
 }
